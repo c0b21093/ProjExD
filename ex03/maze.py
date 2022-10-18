9d80f6552a66823f9bb800ff83981d1189b972b8
keisuke <c0b21093f8@edu.teu.ac.jp>
2022-10-18 14:54:35 +0900

    pra7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,20 +1,66 @@
 import tkinter as tk
+import maze_maker
 
-global cx, key
-global cy
 
-root = tk.Tk()
-root.title("迷える工科トン")
-root.geometry("1500x900")
+def key_down(event):
+    global key
+    key = event.keysym
+    #print(key)
 
-canvas = tk.Canvas(root, background="#000", width=1500, height=900)
-canvas.pack()
 
-img = tk.PhotoImage(file="fig/0.png")
-cx = 300
-cy = 400
-key = ""
+def key_up(event):
+    global key
 
-canvas.create_image(cx, cy, image=img, tag="tori")
+    key = ""
 
-root.mainloop()
+
+def main_proc():
+    global cx, cy
+
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Left":
+        cx -= 20
+    elif key == "Right":
+        cx += 20
+
+    print(cx,cy)    
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
+def make_maze(15, 9):
+    
+
+
+if __name__ == "__main__":
+
+    global cx
+    global cy
+
+    root = tk.Tk()
+    root.title("迷える工科トン")
+    root.geometry("1500x900")
+
+    canvas = tk.Canvas(root, background="#000", width=1500, height=900)
+    canvas.pack()
+
+    img = tk.PhotoImage(file="fig/0.png")
+    cx = 300
+    cy = 400
+
+    canvas.create_image(cx, cy, image=img, tag="tori")
+    
+    key = ""
+
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    main_proc()
+
+    root.mainloop()
+
+    
+
