e56c67c7068e90d4d65fc599f60e537632590c5c
keisuke <c0b21093f8@edu.teu.ac.jp>
2022-10-18 14:18:54 +0900

    pra3

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,10 +1,19 @@
 import tkinter as tk
 
+global cx
+global cy
+
 root = tk.Tk()
 root.title("迷える工科トン")
 root.geometry("1500x900")
 
 canvas = tk.Canvas(root, background="#000", width=1500, height=900)
 canvas.pack()
+
+img = tk.PhotoImage(file="fig/0.png")
+cx = 300
+cy = 400
+
+canvas.create_image(cx, cy, image=img tag="tori")
 
 root.mainloop()
