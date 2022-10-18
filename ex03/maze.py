a5990ab139a3b4fb60fb61016a7cf5cc6b62c216
keisuke <c0b21093f8@edu.teu.ac.jp>
2022-10-18 15:53:06 +0900

    pra12

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,56 +10,51 @@ def key_down(event):
 
 def key_up(event):
     global key
-
     key = ""
 
 
 def main_proc():
+    delta = {
+        
+        ""     : [0,  0], 
+        "Up"   : [0, -1],
+        "Down" : [0, +1],
+        "Left" : [-1, 0],
+        "Right": [+1, 0],
+    }
+    global mx, my
     global cx, cy
 
-    if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -= 20
-    elif key == "Right":
-        cx += 20
-
-    print(cx,cy)    
-    canvas.coords("tori", cx, cy)
+    if maze_list[my+delta[key][1]][mx+delta[key][0]] == 0:
+        mx, my = mx+delta[key][0], my+delta[key][1] 
+        cx, cy = mx*100+50, my*100+50
+    canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
 
-#def make_maze(15, 9):
 
 
 if __name__ == "__main__":
-
-    global cx
-    global cy
-
     root = tk.Tk()
     root.title("迷える工科トン")
     root.geometry("1500x900")
 
-    canvas = tk.Canvas(root, background="#000", width=1500, height=900)
-    canvas.pack()
+    canv = tk.Canvas(root, background="#000", width=1500, height=900)
+    canv.pack()
+
+    maze_list = maze_maker.make_maze(15, 9)
+    maze_maker.show_maze(canv, maze_list)
 
     img = tk.PhotoImage(file="fig/0.png")
-    cx = 300
-    cy = 400
+    mx, my = 1, 1
+    cx, cy = mx * 100 + 50, my * 100 + 50
+    canv.create_image(cx, cy, image=img, tag="tori")
 
-    canvas.create_image(cx, cy, image=img, tag="tori")
-    
     key = ""
 
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
-
-    root.mainloop()
-
     
-
+    root.mainloop()
