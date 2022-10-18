d274304da2f70851da585a6891640d492ee36212
keisuke <c0b21093f8@edu.teu.ac.jp>
2022-10-18 13:57:26 +0900

    pra2

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,4 +3,8 @@ import tkinter as tk
 root = tk.Tk()
 root.title("迷える工科トン")
 root.geometry("1500x900")
+
+canvas = tk.Canvas(root, background="#000", width=1500, height=900)
+canvas.pack()
+
 root.mainloop()
