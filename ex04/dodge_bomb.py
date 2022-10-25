59f735b8e37f16d3388228b2f2a3eb66c6f63b1d
keisuke <c0b21093f8@edu.teu.ac.jp>
2022-10-25 14:26:53 +0900

    pra4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,6 +3,7 @@ import pygame as pg
 import sys
 
 def main():
+    tori_x, tori_y = 900, 400
     pg.init()
     pg.display.set_mode((1600,900), 0, 32)
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,10 +13,10 @@ def main():
     bg = pg.image.load("fig/haikei.png")
     rect_bg = bg.get_rect()
 
-    tori = pg.image.load("fig/0.png")
+    tori = pg.image.load("fig/6.png")
     tori = pg.transform.rotozoom(tori, 0, 2.0)
     rect_tori = tori.get_rect()
-    rect_tori.center = (900,400)
+    rect_tori.center = (tori_x, tori_y)
 
     while(1):
         screen.blit(bg, rect_bg)
@@ -25,6 +26,15 @@ def main():
             if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+            if event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
+                rect_tori.move_ip(0, 10)
+            if event.type == pg.KEYDOWN and event.key == pg.K_UP:
+                rect_tori.move_ip(0, -10)
+            if event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
+                rect_tori.move_ip(-10, 0)
+            if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
+                rect_tori.move_ip(10, 0)
+
         
         pg.display.update()
         clock = pg.time.Clock()
